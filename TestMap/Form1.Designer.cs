bb8ce978cffaf9c38bbd02f6b7c53d952a237a50
quang.tran1 <quang.tran1@mht.masangroup.com>
2023-06-24 22:22:36 +0700

    Add project files.

---
+++ TestMap/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace TestMap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gmap = new GMap.NET.WindowsForms.GMapControl();
+            this.txtCountryName = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtCityName = new System.Windows.Forms.TextBox();
+            this.txtLong = new System.Windows.Forms.TextBox();
+            this.txtLat = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.nud_minzoom = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.nud_maxzoom = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.nud_zoom = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_minzoom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_maxzoom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_zoom)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gmap
+            // 
+            this.gmap.Bearing = 0F;
+            this.gmap.CanDragMap = true;
+            this.gmap.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gmap.GrayScaleMode = false;
+            this.gmap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gmap.LevelsKeepInMemory = 5;
+            this.gmap.Location = new System.Drawing.Point(12, 78);
+            this.gmap.MarkersEnabled = true;
+            this.gmap.MaxZoom = 18;
+            this.gmap.MinZoom = 2;
+            this.gmap.MouseWheelZoomEnabled = true;
+            this.gmap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gmap.Name = "gmap";
+            this.gmap.NegativeMode = false;
+            this.gmap.PolygonsEnabled = true;
+            this.gmap.RetryLoadTile = 0;
+            this.gmap.RoutesEnabled = true;
+            this.gmap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gmap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gmap.ShowTileGridLines = false;
+            this.gmap.Size = new System.Drawing.Size(776, 360);
+            this.gmap.TabIndex = 0;
+            this.gmap.Zoom = 13D;
+            // 
+            // txtCountryName
+            // 
+            this.txtCountryName.Location = new System.Drawing.Point(72, 12);
+            this.txtCountryName.Name = "txtCountryName";
+            this.txtCountryName.Size = new System.Drawing.Size(119, 20);
+            this.txtCountryName.TabIndex = 1;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtCityName
+            // 
+            this.txtCityName.Location = new System.Drawing.Point(72, 38);
+            this.txtCityName.Name = "txtCityName";
+            this.txtCityName.Size = new System.Drawing.Size(119, 20);
+            this.txtCityName.TabIndex = 3;
+            // 
+            // txtLong
+            // 
+            this.txtLong.Location = new System.Drawing.Point(280, 38);
+            this.txtLong.Name = "txtLong";
+            this.txtLong.Size = new System.Drawing.Size(119, 20);
+            this.txtLong.TabIndex = 5;
+            // 
+            // txtLat
+            // 
+            this.txtLat.Location = new System.Drawing.Point(280, 12);
+            this.txtLat.Name = "txtLat";
+            this.txtLat.Size = new System.Drawing.Size(119, 20);
+            this.txtLat.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Country";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "City Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(219, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Long";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(220, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Lat";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(418, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(59, 46);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // nud_minzoom
+            // 
+            this.nud_minzoom.Location = new System.Drawing.Point(501, 38);
+            this.nud_minzoom.Name = "nud_minzoom";
+            this.nud_minzoom.Size = new System.Drawing.Size(69, 20);
+            this.nud_minzoom.TabIndex = 11;
+            this.nud_minzoom.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(498, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Min Zoom";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(588, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Max Zoom";
+            // 
+            // nud_maxzoom
+            // 
+            this.nud_maxzoom.Location = new System.Drawing.Point(591, 38);
+            this.nud_maxzoom.Name = "nud_maxzoom";
+            this.nud_maxzoom.Size = new System.Drawing.Size(69, 20);
+            this.nud_maxzoom.TabIndex = 13;
+            this.nud_maxzoom.Value = new decimal(new int[] {
+            18,
+            0,
+            0,
+            0});
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(682, 22);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Zoom";
+            // 
+            // nud_zoom
+            // 
+            this.nud_zoom.Location = new System.Drawing.Point(685, 38);
+            this.nud_zoom.Name = "nud_zoom";
+            this.nud_zoom.Size = new System.Drawing.Size(69, 20);
+            this.nud_zoom.TabIndex = 15;
+            this.nud_zoom.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.nud_zoom);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.nud_maxzoom);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.nud_minzoom);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLong);
+            this.Controls.Add(this.txtLat);
+            this.Controls.Add(this.txtCityName);
+            this.Controls.Add(this.txtCountryName);
+            this.Controls.Add(this.gmap);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nud_minzoom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_maxzoom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_zoom)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl gmap;
+        private System.Windows.Forms.TextBox txtCountryName;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtCityName;
+        private System.Windows.Forms.TextBox txtLong;
+        private System.Windows.Forms.TextBox txtLat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown nud_minzoom;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown nud_maxzoom;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown nud_zoom;
+    }
+}
+
---
+++ TestMap/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GMap;
+using GMap.NET.WindowsForms;
+
+namespace TestMap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string ExcelPath = @"H:\Desktop\FreeLancer\Bussiness Process\XLSX_OUTPUT\FL-Miami-6-7-23-1023AM.xlsx";
+        public DataTable Excel_data;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Excel_data = ReadExcelFile("data-A", ExcelPath);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gmap.MaxZoom = (int)(nud_maxzoom.Value);
+            gmap.MinZoom = (int)(nud_minzoom.Value);
+            gmap.Zoom = (int)(nud_zoom.Value);
+            gmap.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
+            gmap.Position = new GMap.NET.PointLatLng(double.Parse(txtLat.Text), double.Parse(txtLong.Text));
+
+
+            DataTable newTable = new DataTable("NewTable");
+            newTable.Columns.Add("CityName", typeof(string));
+            newTable.Columns.Add("lat", typeof(double));
+            newTable.Columns.Add("long", typeof(double));
+
+            foreach (DataRow row in Excel_data.Rows)
+            {
+                string City = (string)row["CITY"];
+                double lat = (double)row["LATITUDE"];
+                double lng = (double)row["LONGITUDE"];
+                newTable.Rows.Add(City,lat, lng);
+            }
+
+            foreach(DataRow dtrow in newTable.Rows)
+            {
+                GMap.NET.WindowsForms.GMapOverlay markers = new GMap.NET.WindowsForms.GMapOverlay("markers");
+                GMap.NET.WindowsForms.GMapMarker marker = new GMap.NET.WindowsForms.Markers.GMarkerGoogle(
+                                                                new GMap.NET.PointLatLng(dtrow.Field<double>("lat"), dtrow.Field<double>("long")),
+                                                                            GMap.NET.WindowsForms.Markers.GMarkerGoogleType.red_small);
+                markers.Markers.Add(marker);
+                gmap.Overlays.Add(markers);
+            }
+
+           
+        }
+
+
+        private static DataTable ReadExcelFile(string sheetName, string path)
+        {
+            using (OleDbConnection conn = new OleDbConnection())
+            {
+                DataTable dt = new DataTable();
+                string Import_FileName = path;
+                string fileExtension = Path.GetExtension(Import_FileName);
+                conn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + Import_FileName + ";" + "Extended Properties='Excel 12.0 Xml;HDR=YES;'";
+                using (OleDbCommand comm = new OleDbCommand())
+                {
+                    comm.CommandText = "Select * from [" + sheetName + "$]";
+                    comm.Connection = conn;
+                    using (OleDbDataAdapter da = new OleDbDataAdapter())
+                    {
+                        da.SelectCommand = comm;
+                        da.Fill(dt);
+                        comm.Dispose();
+                        return dt;
+                    }
+                }
+            }
+        }
+    }
+}
